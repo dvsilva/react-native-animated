43903fc3a90af1bb6be5bf4badd126a791c462f5
Vinicios Neves <vinicios.neves@mac>
2024-10-19 14:37:52 +0100

    Video 3

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,61 +1,27 @@
-import { useRef } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import { Animated } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function TabTwoScreen() {
 
-  const fadeAnim = useRef(new Animated.Value(0)).current
-  const moveAnim = useRef(new Animated.Value(0)).current
-  const scaleAnim = useRef(new Animated.Value(0)).current
+  const fadeAnim = useSharedValue(0)
+  const reanimatedStyle =  useAnimatedStyle(() => {
+    return {
+      opacity: fadeAnim.value
+    }
+  })
 
   const fadeIn = () => {
-    const fadeAnimation = Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 3000,
-      useNativeDriver: true
-    })
-    const moveAnimation = Animated.timing(moveAnim, {
-      toValue: 200,
-      duration: 3000,
-      useNativeDriver: true
-    })
-    const scaleAnimation = Animated.timing(scaleAnim, {
-      toValue: 2,
-      duration: 3000,
-      useNativeDriver: true
-    })
-    Animated.parallel([fadeAnimation, moveAnimation, scaleAnimation]).start()
+    fadeAnim.value = withTiming(1, { duration: 3000 })
   }
 
   const fadeOut = () => {
-    const fadeAnimation = Animated.timing(fadeAnim, {
-      toValue: 0,
-      duration: 3000,
-      useNativeDriver: true
-    })
-    const moveAnimation = Animated.timing(moveAnim, {
-      toValue: 0,
-      duration: 3000,
-      useNativeDriver: true
-    })
-    const scaleAnimation = Animated.timing(scaleAnim, {
-      toValue: 0,
-      duration: 3000,
-      useNativeDriver: true
-    })
-    Animated.parallel([fadeAnimation, moveAnimation, scaleAnimation]).start()
+    fadeAnim.value = withTiming(0, { duration: 6000 })
   }
 
   return (
     <SafeAreaView style={styles.container}>
-      <Animated.View style={[styles.faddingContainer, {
-        opacity: fadeAnim,
-        transform: [
-          { translateY: moveAnim },
-          { scale: scaleAnim }
-        ]
-      }]}>
+      <Animated.View style={[styles.faddingContainer, reanimatedStyle]}>
         <Text>
           Olá mundo das animações
         </Text>
